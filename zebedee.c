beeef55b7178af487a0a370b92fd3c2d723590f3
Roman Hoog Antink <rha@open.ch>
2010-09-01 11:11:02 +0200

    fixed compilation on solaris 10 / sparc
    found solution here: https://lists.balabit.hu/pipermail/syslog-ng/2007-January/009689.html

--- zebedee.c
+++ zebedee.c
@@ -167,6 +167,11 @@ extern int svcRemove(char *name);
 #define INADDR_NONE 0xffffffff
 #endif
 
+/* Solaris 10 */
+#ifndef s6_addr32
+#define s6_addr32       _S6_un._S6_u32
+#endif
+
 #define DFLT_SHELL	"/bin/sh"
 #define FILE_SEP_CHAR	'/'
 
@@ -8378,46 +8383,6 @@ switchUser(void)
 **
 ** Compare two sockaddr structures after applying mask to each one of them.
 ** Return 0 if both addresses match; or -1 otherwise.
-*/
-
-/* FIXME Solaris 10 / Sparc "SunOS medusa10 5.10 Generic_125100-05 sun4u sparc SUNW,Sun-Fire-V240"
-
-in6_addr32 only available, if _KERNEL macro is defined.
-
-from /usr/include/netinet/in.h:
-
-struct in6_addr {
-        union {
-                / *
-                 * Note: Static initalizers of "union" type assume
-                 * the constant on the RHS is the type of the first member
-                 * of union.
-                 * To make static initializers (and efficient usage) work,
-                 * the order of members exposed to user and kernel view of
-                 * this data structure is different.
-                 * User environment sees specified uint8_t type as first
-                 * member whereas kernel sees most efficient type as
-                 * first member.
-                 * /
-#ifdef _KERNEL
-                uint32_t        _S6_u32[4];     /* IPv6 address * /
-                uint8_t         _S6_u8[16];     /* IPv6 address * /
-#else
-                uint8_t         _S6_u8[16];     /* IPv6 address * /
-                uint32_t        _S6_u32[4];     /* IPv6 address * /
-#endif
-                uint32_t        __S6_align;     /* Align on 32 bit boundary * /
-        } _S6_un;
-};
-#define s6_addr         _S6_un._S6_u8
-
-#ifdef _KERNEL
-#define s6_addr8        _S6_un._S6_u8
-#define s6_addr32       _S6_un._S6_u32
-#endif
-
-typedef struct in6_addr in6_addr_t;
-
 */
 
 int 
