2dc07d4eddf30844906bf405c0da2862b11d25cc
Roman Hoog Antink <rha@open.ch>
2011-02-03 12:44:09 +0100

    prepare version 2.5.5

--- zebedee.c
+++ zebedee.c
@@ -22,7 +22,7 @@
 */
 
 char *zebedee_c_rcsid = "$Id: zebedee.c,v 1.49 2005-09-02 22:20:23 ndwinton Exp $";
-#define RELEASE_STR "2.5.4"
+#define RELEASE_STR "2.5.5"
 
 #include <stdio.h>
 #include <stdlib.h>
